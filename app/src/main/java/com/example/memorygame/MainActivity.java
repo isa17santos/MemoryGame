69c6c28ea6491a31a0e3a50ff9ca3be2bc6c14cb
isasantos17 <2221408@my.ipleiria.pt>
2024-11-10 11:28:36 +0000

    finish_dashboardUser_AND_implement_boardSize_page

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.memorygame;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void moveTo_boardsize_page (View view)
+    {
+        setContentView(R.layout.boardsize_page);
+    }
+
+    public void moveTo_history_page (View view)
+    {
+        setContentView(R.layout.history_page);
+    }
+
+    public void moveTo_scoreboard_page (View view)
+    {
+        setContentView(R.layout.scoreboard_page);
+    }
+
+    public void moveTo_dashboard_user (View view)
+    {
+        setContentView(R.layout.dashboard_user);
+    }
+
+    public void moveTo_board3x4 (View view)
+    {
+        setContentView(R.layout.board3x4);
+    }
+
+    public void moveTo_board4x4 (View view)
+    {
+        setContentView(R.layout.board4x4);
+    }
+
+    public void moveTo_board6x6 (View view)
+    {
+        setContentView(R.layout.board6x6);
     }
 }
