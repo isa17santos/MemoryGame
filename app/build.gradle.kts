18cb82f99344628d405ec7f1f3b64783e4781603
isasantos17 <2221408@my.ipleiria.pt>
2024-11-10 17:02:44 +0000

    Revert "board3x4"
    This reverts commit 954708aa1d30c30be24ee206d1181c8bf9cda6be.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
     namespace = "com.example.memorygame"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.memorygame"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,9 +29,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
 }
 
 dependencies {
@@ -41,7 +37,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.memorygame;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void moveTo_boardsize_page (View view)
+    {
+        setContentView(R.layout.boardsize_page);
+    }
+
+    public void moveTo_history_page (View view)
+    {
+        setContentView(R.layout.history_page);
+    }
+
+    public void moveTo_scoreboard_page (View view)
+    {
+        setContentView(R.layout.scoreboard_page);
+    }
+
+    public void moveTo_dashboard_user (View view)
+    {
+        setContentView(R.layout.dashboard_user);
+    }
+
+    public void moveTo_board3x4 (View view)
+    {
+        setContentView(R.layout.board3x4);
+    }
+
+    public void moveTo_board4x4 (View view)
+    {
+        setContentView(R.layout.board4x4);
+    }
+
+    public void moveTo_board6x6 (View view)
+    {
+        setContentView(R.layout.board6x6);
+    }
+}
